037f2850c19359bfcba4d7e781012c256905af2a
dorjepadma <dorjepadma@gmail.com>
2022-02-01 23:49:05 -0800

    forcing build pack

--- src/components/spinningCube/spinningCube.component.jsx
+++ src/components/spinningCube/spinningCube.component.jsx
@@ -7,7 +7,7 @@ function SpinningCube() {
   <div className="cube">
   <div className="top">Y</div>
   <div className="right">Metaverse</div>
-  <div className="bottom">Z</div>
+  <div className="bottom">Zeta</div>
   <div className="left">Space</div>
   <div className="front">Time</div>
   <div className="back">X</div>
