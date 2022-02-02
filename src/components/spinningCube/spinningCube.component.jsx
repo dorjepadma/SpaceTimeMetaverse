39bdbbe3ea377583e3914221a811a057bf9f184e
dorjepadma <dorjepadma@gmail.com>
2022-02-02 00:19:18 -0800

    policy id added

--- src/components/spinningCube/spinningCube.component.jsx
+++ src/components/spinningCube/spinningCube.component.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Typography from '@mui/material/Typography';
 
 import './spinningCube.styles.css'
 
 function SpinningCube() {
   return (
+    
+
   <div className="cube">
   <div className="top" >Y</div>
   <div className="right">Metaverse</div>
@@ -11,7 +14,11 @@ function SpinningCube() {
   <div className="left">Space</div>
   <div className="front">Time</div>
   <div className="back">X</div>
+<Typography variant="h4" color='white' >
+  Policy ID: Space Time Metaverse
+</Typography>
   </div>
+  
 )}
 
 export default SpinningCube;
