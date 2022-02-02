2536388d57d391e0c6772495c796fb4306253ba3
dorjepadma <dorjepadma@gmail.com>
2022-02-02 00:07:27 -0800

    box shadow added font size increased

--- src/components/spinningCube/spinningCube.component.jsx
+++ src/components/spinningCube/spinningCube.component.jsx
@@ -5,9 +5,9 @@ import './spinningCube.styles.css'
 function SpinningCube() {
   return (
   <div className="cube">
-  <div className="top">Y</div>
+  <div className="top" >Y</div>
   <div className="right">Metaverse</div>
-  <div className="bottom">Zeta</div>
+  <div className="bottom">Z</div>
   <div className="left">Space</div>
   <div className="front">Time</div>
   <div className="back">X</div>
